1b68046994e3b7f21b9ac4a1cded451472df4341
Yasz Avellia <zakamaragames@gmail.com>
2022-10-21 21:45:13 +0700

    menambah unction.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 ?>
 
 <!DOCTYPE html>
@@ -272,15 +273,15 @@
                     <form action="" method="post">
                         <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Nama</label>
-                            <input type="email" class="form-control" id="exampleFormControlInput1" />
+                            <input name="nama" type="text" class="form-control" id="exampleFormControlInput1" autocomplete="off" />
                         </div>
                         <div class="mb-3">
                             <label for="exampleFormControlInput1" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                            <input name="email" type="email" class="form-control" id="exampleFormControlInput1" autocomplete="off" placeholder="name@example.com" />
                         </div>
                         <div class="mb-3">
                             <label for="exampleFormControlTextarea1" class="form-label">Pesan</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <textarea name="pesan" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
                         <div class="mb-">
                             <button class="btn btn-primary" type="submit">Kirim</button>
